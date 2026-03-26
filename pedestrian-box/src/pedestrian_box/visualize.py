904f0ab90b614b002a8dc9403e2cdbd0f0f4d02b
Anatolii Skovitin <skovitin.anatolii@gmial.com>
2026-03-26 16:06:46 +0100

    show only pedestrians predictions

--- pedestrian-box/src/pedestrian_box/visualize.py
+++ pedestrian-box/src/pedestrian_box/visualize.py
@@ -15,20 +15,21 @@ def draw_detections(image, detections, color=(255, 80, 80), line_width=3):
     font = ImageFont.load_default()
 
     for detection in detections:
-        x_min, y_min, x_max, y_max = detection["bbox_xyxy"]
-        draw.rectangle([x_min, y_min, x_max, y_max], outline=color, width=line_width)
+        if detection.get("class_name") == "person":
+            x_min, y_min, x_max, y_max = detection["bbox_xyxy"]
+            draw.rectangle([x_min, y_min, x_max, y_max], outline=color, width=line_width)
 
-        caption = _format_caption(detection)
-        text_bbox = draw.textbbox((x_min, y_min), caption, font=font)
-        text_width = text_bbox[2] - text_bbox[0]
-        text_height = text_bbox[3] - text_bbox[1]
-        text_top = max(0, y_min - text_height - 6)
-        text_bottom = text_top + text_height + 4
+            caption = _format_caption(detection)
+            text_bbox = draw.textbbox((x_min, y_min), caption, font=font)
+            text_width = text_bbox[2] - text_bbox[0]
+            text_height = text_bbox[3] - text_bbox[1]
+            text_top = max(0, y_min - text_height - 6)
+            text_bottom = text_top + text_height + 4
 
-        draw.rectangle(
-            [x_min, text_top, x_min + text_width + 6, text_bottom],
-            fill=color,
-        )
-        draw.text((x_min + 3, text_top + 2), caption, fill="white", font=font)
+            draw.rectangle(
+                [x_min, text_top, x_min + text_width + 6, text_bottom],
+                fill=color,
+            )
+            draw.text((x_min + 3, text_top + 2), caption, fill="white", font=font)
 
     return rendered
